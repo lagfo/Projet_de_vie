0d20f0a421400a0987ee698948e0c13c9d395971
Omar Sawadogo <sawomar226@gmail.com>
2025-05-13 15:47:31 +0000

    commit project 13_05_2025 15h46

--- app/src/main/java/org/ticanalyse/projetdevie/presentation/main/MainActivity.kt
+++ app/src/main/java/org/ticanalyse/projetdevie/presentation/main/MainActivity.kt
@@ -3,27 +3,18 @@ package org.ticanalyse.projetdevie.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import org.ticanalyse.projetdevie.presentation.nvgraph.NavGraph
 import org.ticanalyse.projetdevie.presentation.nvgraph.Route
-import org.ticanalyse.projetdevie.presentation.splash.SplashScreen
 import org.ticanalyse.projetdevie.ui.theme.ProjetDeVieTheme
 
 @AndroidEntryPoint
