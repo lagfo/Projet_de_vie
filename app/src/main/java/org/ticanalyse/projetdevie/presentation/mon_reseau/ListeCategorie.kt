ad0a0838a69d20e5d99d7538fa3fc3ef2bf1ed0d
Omar Sawadogo <59999106+OmarSawadogo@users.noreply.github.com>
2025-06-04 19:24:59 +0000

    commit project 04_06_2025 19h24

--- app/src/main/java/org/ticanalyse/projetdevie/presentation/mon_reseau/ListeCategorie.kt
+++
@@ -1,9 +0,0 @@
-package org.ticanalyse.projetdevie.presentation.mon_reseau
-
-import androidx.annotation.DrawableRes
-
-data class ListeCategorie(
-    val title: String,
-    val route: String,
-    @DrawableRes val image: Int
-)
--- app/src/main/java/org/ticanalyse/projetdevie/presentation/mon_reseau/ListeSousCategorie.kt
+++
@@ -1,9 +0,0 @@
-package org.ticanalyse.projetdevie.presentation.mon_reseau
-
-import androidx.annotation.DrawableRes
-
-data class ListeSousCategorie(
-    val title: String,
-    val route: String,
-    @DrawableRes val image: Int
-)
